d6985b8b5629b98de18dfd50fbb754c0adcc5cdf
gonzalovm9 <52208722+gonzalovm9@users.noreply.github.com>
2026-03-16 20:21:38 -0500

    Actualizacion Rutas

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from "@angular/router";
 
 export const AUTH_ROUTES: Routes = [
-    { path: 'login', loadComponent: () => import('./pages/login-page/login-page').then(m => m.LoginPage), title: 'Login' },
+    { path: 'login', loadComponent: () => import('./pages/login-page/login-page').then(m => m.LoginPage), title: 'NPS - Login' },
 ];
